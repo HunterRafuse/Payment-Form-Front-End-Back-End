e6254dac9d260e5feff81d76b1a1a02fb466096c
Hunter Rafuse <31886722+HunterRafuse@users.noreply.github.com>
2018-04-18 10:31:34 +0300

    Create db.php
    Database info for connection.

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+  // DB Params
+  define("DB_HOST", "localhost");  //what we are testing on
+  define("DB_USER", "root");  //user
+  define("DB_PASS", "PASS");
+  define("DB_NAME", "paypage"); //database name
